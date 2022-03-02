732ffb2de1ba27fc94bac30b88a459853eb4e555
tjschulz <tjschulz@hansalog.de>
2022-03-02 08:19:47 +0100

    API

--- main.py
+++ main.py
@@ -1,13 +1,73 @@
 # open cmd and: "cd C:\Users\tjschulz\Desktop\Schule\Aufgaben\Netzwerktechnik\Lehrjahr 2\2022\RestAPI"
 #         then: "py main.py"
 
-from flask import Flask, render_template      
+import uuid 
+
+from flask import Flask, render_template, request, jsonify, abort     
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("main.html")
+user_id_bob = uuid.uuid4()
+user_id_alice = uuid.uuid4()
+user_id_eve = uuid.uuid4()
+todo_list_1_id = '1'
+todo_list_2_id = '2'
+todo_list_3_id = '3'
+todo_1_id = uuid.uuid4()
+todo_2_id = uuid.uuid4()
+todo_3_id = uuid.uuid4()
+todo_4_id = uuid.uuid4()
+
+# define internal data structures with example data
+user_list = [
+    {'id': user_id_bob, 'name': 'Bob'},
+    {'id': user_id_alice, 'name': 'Alice'},
+    {'id': user_id_eve, 'name': 'Eve'},
+]
+todo_lists = [
+    {'id': todo_list_1_id, 'name': 'Einkaufsliste'},
+    {'id': todo_list_2_id, 'name': 'Arbeit'},
+    {'id': todo_list_3_id, 'name': 'Privat'},
+]
+todos = [
+    {'id': todo_1_id, 'name': 'Milch', 'description': '', 'list': todo_list_1_id, 'user': user_id_bob},
+    {'id': todo_2_id, 'name': 'Arbeitsblätter ausdrucken', 'description': '', 'list': todo_list_2_id, 'user': user_id_alice},
+    {'id': todo_3_id, 'name': 'Kinokarten kaufen', 'description': '', 'list': todo_list_3_id, 'user': user_id_eve},
+    {'id': todo_3_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id, 'user': user_id_eve},
+]
+
+# add some headers to allow cross origin access to the API on this server, necessary for using preview in Swagger Editor!
+@app.after_request
+def apply_cors_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+# define endpoint for getting and deleting existing todo lists
+@app.route('/list/<list_id>', methods=['GET', 'DELETE'])
+def handle_list(list_id):
+    # find todo list depending on given list id
+    list_item = None
+    for l in todo_lists:
+        if l['id'] == list_id:
+            list_item = l
+            break
+    # if the given list id is invalid, return status code 404
+    if not list_item:
+        abort(404)
+    if request.method == 'GET':
+        # find all todo entries for the todo list with the given id
+        print('Returning todo list...')
+        return jsonify([i for i in todos if i['list'] == list_id])
+    elif request.method == 'DELETE':
+        # delete list with given id
+        print('Deleting todo list...')
+        todo_lists.remove(list_item)
+        return '', 200
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
